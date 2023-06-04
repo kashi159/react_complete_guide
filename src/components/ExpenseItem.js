3b7796f3b3d6cdbf38c5a902a973c6859a404e45
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-06-04 10:44:35 +0530

    Task 7 Lets start coding

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,20 @@
 function ExpenseItem() {
-    return(
-    
-    <div> 
+  return (
+    <div>
+      <div>
         <h2>Expense item!</h2>
+      </div>
+      <div>
         <ul>
+          <div>
             <li>Food Rs 10</li>
             <li>Petrol Rs 100</li>
             <li>Movies Rs 200</li>
+          </div>
         </ul>
+      </div>
     </div>
-    
-    
-    ) 
+  );
 }
 
 export default ExpenseItem;
