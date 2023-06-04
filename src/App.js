ecde205ed29cdb77126301274edda517718cc258
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-06-04 15:05:36 +0530

    Task 10 commit Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
       date: new Date(2020, 7, 14),
       location: "Sanitary",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), location: "Appliance" },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Appliance",
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -36,7 +42,6 @@ function App() {
           location={expense.location}
         />
       ))}
-      
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
-  
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__location">{props.location}</div>
-        <div className="expense-item__price">Rs {props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+      />
     </div>
   );
 }
