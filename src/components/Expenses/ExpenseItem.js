3d0976e6fa86e249cadd8d80f3b12c9d2d92a954
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2023-06-05 10:41:42 +0530

    Advance React Task 2 Commit Manipulating the DOM - React States

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked!!");
+    setTitle('Updated!')
   };
+
+  const [price, setPrice] = useState(props.amount)
+
+  const clickHandlerPrice = () =>{
+    setPrice(100)
+  }
 
   const deleteExpense = (e) => {
     const listItem = e.target.closest(".expense-item");
@@ -19,11 +28,12 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         id={props.id}
-        amount={props.amount}
+        amount={price}
         location={props.location}
-        title={props.title}
+        title={title}
       />
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={clickHandlerPrice}>Change Price</button>
       <button onClick={deleteExpense}>Delete</button>
     </Card>
   );
